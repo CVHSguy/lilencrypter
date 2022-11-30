6c7d0bc4506430072ce292d57f4815d075e95ae2
CVHSguy <101105190+CVHSguy@users.noreply.github.com>
2022-11-30 09:09:17 +0100

    Add project files.

---
+++ yea/Program.cs
@@ -0,0 +1,11 @@
+﻿using yea;
+
+string password = "123hello";
+
+
+string encrypter = pshand.Command($"ConvertTo-SecureString '{password}' -AsPlainText -Force | ConvertFrom-SecureString");
+Console.WriteLine("encrypting the password: "+encrypter);
+
+
+string decrypter = pshand.Command($"[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($('{encrypter}'| ConvertTo-SecureString)))");
+Console.WriteLine("decrypting the password: " +decrypter);
---
+++ yea/pshand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yea
+{
+    public static class pshand
+    {
+        private static PowerShell ps = PowerShell.Create();
+
+        public static string Command(string command)
+        {
+
+            string output = string.Empty;
+            ps.AddScript(command);
+
+
+            PSDataCollection<PSObject> outputCollection = new();
+            ps.AddCommand("Out-String");
+
+            IAsyncResult result = ps.BeginInvoke<PSObject, PSObject>(null, outputCollection); 
+
+            ps.EndInvoke(result);
+
+            StringBuilder sb = new StringBuilder();
+            foreach(var outputItem in outputCollection)
+            {
+                sb.AppendLine(outputItem.BaseObject.ToString());
+
+            }
+            ps.Commands.Clear();
+
+            return sb.ToString().Trim();
+
+
+        }
+
+
+
+
+    }
+}
